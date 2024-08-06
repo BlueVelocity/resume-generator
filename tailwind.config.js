d650cbdb8874e5fb9a4efcf4233c2ccd59c07ad4
BlueVelocity <josephrodney@hotmail.com>
2024-08-06 10:54:53 -0400

    Add color scheme config and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#e2e8f0",
+        main: "#1e293b",
+        secondary: "#0f172a",
+        accent: "#ea580c",
+      },
+    },
   },
   plugins: [],
 };
